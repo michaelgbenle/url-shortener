e888f77cc66bc61dcdd6a340d7cf424f78567d44
Michael <michaelgbenle@gmail.com>
2022-07-11 10:23:44 +0100

    url shortener

--- models/model.go
+++ models/model.go
@@ -4,4 +4,5 @@ type Short struct {
 	ID       uint64 `json:"id" gorm:"primaryKey"`
 	Redirect string `json:"redirect"`
 	Shorted  string `json:"shorted" gorm:"unique;not null"`
+	Clicked  string `json:"clicked" gorm:"primaryKey"`
 }
