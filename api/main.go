33316fd846ab183a6c89372f3dfe71273f597033
michaelgbenle <michaelgbenle@gmail.com>
2022-07-24 09:37:42 -0700

    work in progress

--- api/main.go
+++ api/main.go
@@ -3,11 +3,12 @@ package main
 import "github.com/michaelgbenle/url-shortener/routes"
 
 func main() {
-
+app:= fiber.New()
 }
 
 func SetupRoutes(app *fiber.App) {
 	app.Get("/:url", routes.ResolveUrl)
 	app.Post("/api/v1/", routes.ShortenUrl)
+
 
 }
