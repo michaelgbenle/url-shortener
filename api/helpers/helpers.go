cdbba06a43e4a94ac001d456e93debcca95b2745
michaelgbenle <michaelgbenle@gmail.com>
2022-07-28 07:33:17 -0700

    url shortener

--- api/helpers/helpers.go
+++ api/helpers/helpers.go
@@ -3,7 +3,9 @@ package helpers
 import "net/url"
 
 func EnforceHTTP() string {
-	if url[:4] !=  "http"
+	if url[:4] !=  "http" {
+		return "http://" + url
+	}
 }
 
 func RemoveDomainError(url string) bool {
