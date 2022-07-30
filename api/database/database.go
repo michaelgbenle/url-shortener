11f03a9517b52f33bc0f1708c2c3c05bc59020d5
michaelgbenle <michaelgbenle@gmail.com>
2022-07-30 12:40:27 -0700

    url shortener

--- api/database/database.go
+++ api/database/database.go
@@ -15,7 +15,7 @@ var Ctx = context.Background()
 func CreateClient(dbNo int) *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
 		Addr: os.Getenv("DB_ADDR"),
-		Password:,
+		Password:os.Getenv("DB_PASS"),
 		DB: dbNo,
 	})
 }
