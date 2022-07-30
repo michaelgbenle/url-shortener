1e4546fd52cd006f64923dcfc6ff4aeb7f6bafe2
michaelgbenle <michaelgbenle@gmail.com>
2022-07-30 13:28:18 -0700

    url shortener

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -43,7 +43,7 @@ func ShortenUrl(c *fiber.Ctx) error {
 			limit,_:=r2.TTL(database.Ctx,c.IP()).Result()
 			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
 				"error":"Rate limit exceeded",
-				"rate_limit_rest":
+				"rate_limit_rest": limit/time.Nanosecond/
 			})
 		}
 	}
