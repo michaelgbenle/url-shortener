7666bc4a879221443eabb0171fc5a7d34bc6a3c9
michaelgbenle <michaelgbenle@gmail.com>
2022-07-30 15:13:00 -0700

    url shortener

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -82,7 +82,7 @@ if body.Expiry ==0{
 err = r.Set(database.Ctx,id,body.URL,body.Expiry*3600*time.Second).Err()
 		if err != nil{
 			c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-				"error":"",
+				"error":"unable to connect to server",
 			})
 		}
 
