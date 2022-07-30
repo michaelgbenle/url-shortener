317cd08e72170f850cc3fad92e2170bf331c0938
michaelgbenle <michaelgbenle@gmail.com>
2022-07-30 12:47:12 -0700

    url shortener

--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -1,8 +1,12 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/michaelgbenle/url-shortener/database"
+)
 
 func ResolveUrl(c *fiber.Ctx) error {
 	url := c.Params("url")
+	r:= database.CreateClient(0)
 
 }
